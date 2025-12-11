6d2f08ec6db8a48cca2a760e840ec69e3dad6723
YoussefBettaieb <youssef.bettaieb@insat.ucar.tn>
2025-12-11 17:53:39 +0100

    copmleted events endpoints without junction

--- src/events/events.controller.ts
+++ src/events/events.controller.ts
@@ -5,18 +5,24 @@ import { CreateEventDto } from './dtos/create-event.dto';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { AdminGuard } from 'src/guards/admin.guard';
 
+@UseGuards(JwtAuthGuard)
 @Controller('/api/events')
 export class EventsController {
   constructor(private eventsService: EventsService) {}
 
-  @UseGuards(JwtAuthGuard)
   @Get()
   async getAllEvents() {
     return this.eventsService.findAll();
   }
-  @UseGuards(JwtAuthGuard, AdminGuard)
+
+  @UseGuards(AdminGuard)
   @Post()
   async createEvent(@Body() body: CreateEventDto) {
     return this.eventsService.create(body);
+  }
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getEventById(@Param('id') id: number) {
+    return this.eventsService.findOne(id);
   }
 }
